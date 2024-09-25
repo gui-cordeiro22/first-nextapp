e1fd23b76814e18040584df52f9772e3b58eac75
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2024-09-25 12:06:42 -0300

    feat: adding ghost loaders on dashboards components

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+// Dependencies
+import React from "react";
+
+// Component
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
